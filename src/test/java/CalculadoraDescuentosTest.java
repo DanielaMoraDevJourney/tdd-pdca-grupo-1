5b42e2482f4f07905c14e399dd8ad65e6a74f341
DanielaMoraDevJourney <162391537+DanielaMoraDevJourney@users.noreply.github.com>
2025-11-24 00:52:04 +0000

    Correciones de ruta e instancias.

--- src/test/java/CalculadoraDescuentosTest.java
+++ src/test/java/CalculadoraDescuentosTest.java
@@ -2,6 +2,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraDescuentosTest {
+    
+    CalculadoraDescuentos calc = new CalculadoraDescuentos();
 
     
 }
